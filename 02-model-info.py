8831590f4c2d7d492cbf533708d199f3d7095ef2
simon gao <little51@126.com>
2026-01-26 12:03:46 +0800

    修改错别字

--- 02-model-info.py
+++ 02-model-info.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolo26n.pt")
 
 # 查看模型信息
-print("模型类别数:", model.names)  # 输出类别名称字典
+print("模型类别:", model.names)  # 输出类别名称字典
 print("总类别数:", len(model.names))  # 输出类别数量
 print("模型信息:")
 print(model.model)  # 查看模型结构
