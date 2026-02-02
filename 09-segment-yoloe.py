1e2aecadc9c137669383fcf61778ce8714249b75
simon gao <little51@126.com>
2026-02-02 13:11:45 +0800

    更新词汇

--- 09-segment-yoloe.py
+++ 09-segment-yoloe.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import matplotlib.pyplot as plt
 
 model = YOLO("yoloe-26l-seg.pt") 
-names = ["person", "car"]
+names = ["person", "black car","computer"]
 model.set_classes(names, model.get_text_pe(names))
 results = model.predict("image05.png")
 plt.imshow(results[0].plot())
