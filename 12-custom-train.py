7cf6856787f7c39886490f98c9abff3993831068
simon gao <little51@126.com>
2026-02-05 12:49:14 +0800

    自定义分类训练

--- 12-custom-train.py
+++ 12-custom-train.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import matplotlib.pyplot as plt
 
 model = YOLO('yolo26n-cls.pt') 
 
@@ -15,4 +16,8 @@ val_results = model.val()
 # 输出验证结果
 print(f"验证准确率: {val_results.top1:.2%}")
 print(f"Top-5准确率: {val_results.top5:.2%}")
-print(f"损失值: {val_results.loss:.4f}")
+
+results = model("image08.jpg")
+plt.imshow(results[0].plot())
+plt.savefig("classification_result.png")
+plt.show()
